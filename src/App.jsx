4e72d9e1856cbbb8e9a8733cbf8dd4631b3987bf
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-12 10:26:18 -0800

    add resetCounter method

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,23 @@ export default class App extends React.Component {
           counter: 0
       }
   }
-    componentWillMount() {
-        setInterval(() => {
-            this.setState({
-                counter: this.state.counter + 1
-            })
-        }, 1000)
-    }
+  componentWillMount() {
+      setInterval(() => {
+          this.setState({
+              counter: this.state.counter + 1
+          })
+      }, 1000)
+  }
+  _resetCounter() {
+    this.setState({
+      counter: 0
+    })
+  }
   render() {
     return (
        <div className="App">
            <h1>hello from {this.props.name}</h1>
-           <h1>Counter value:  {this.state.counter}</h1>
+           <h1 onClick={this._resetCounter.bind(this)}>Counter value:  {this.state.counter}</h1>
        </div>
     );
   }
