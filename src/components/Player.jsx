7ce4482fe3633c9bcfec56603a5c9999479cd6b5
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-12 13:19:46 -0800

    style conditionally based on if player busted

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,10 +7,9 @@ export default class App extends React.Component {
   }
   render() {
     let data = this.props.data;
-    let cardValues = _.sum(_.pluck(data.cards, 'value'))
-    console.log('cardValues: ' + cardValues)
+    let cardValues = _.sum(_.pluck(data.cards, 'value'));
     return (
-       <div className="Player">
+       <div className="Player" style={{color: (cardValues > 21) ? 'red' : 'black'}}>
            <h1 onClick={this.props.handleClick}> Player {data.name}
              with cards:
              {_.map(data.cards, (c) => c.name + " of " + c.suit + " ")}
