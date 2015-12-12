81244674943b73b466fb25910958bf4464929cc2
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-12 10:22:11 -0800

    create a counter that increments every second

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,18 @@ export default class App extends React.Component {
           counter: 0
       }
   }
+    componentWillMount() {
+        setInterval(() => {
+            this.setState({
+                counter: this.state.counter + 1
+            })
+        }, 1000)
+    }
   render() {
     return (
        <div className="App">
-       <h1>hello from {this.props.name}</h1>
+           <h1>hello from {this.props.name}</h1>
+           <h1>Counter value:  {this.state.counter}</h1>
        </div>
     );
   }
