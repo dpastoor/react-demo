be52894053296eaf9e2091af1dbed13bee42ca65
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-12 10:30:57 -0800

    refactor to counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './Components/Counter.jsx';
 import './css/styles.css'
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-          counter: 0
-      }
-  }
-  componentWillMount() {
-      setInterval(() => {
-          this.setState({
-              counter: this.state.counter + 1
-          })
-      }, 1000)
-  }
-  _resetCounter() {
-    this.setState({
-      counter: 0
-    })
   }
   render() {
     return (
-       <div className="App">
-           <h1>hello from {this.props.name}</h1>
-           <h1 onClick={this._resetCounter.bind(this)}>Counter value:  {this.state.counter}</h1>
-       </div>
-    );
+      <div className="App">
+        <h1>hello from {this.props.name}</h1>
+        <Counter />
+        <Counter />
+      </div>
+  );
   }
 }
 
---
+++ src/components/Counter.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+          counter: 0
+      }
+  }
+  componentWillMount() {
+      setInterval(() => {
+          this.setState({
+              counter: this.state.counter + 1
+          })
+      }, 1000)
+  }
+  _resetCounter() {
+    this.setState({
+      counter: 0
+    })
+  }
+  render() {
+    return (
+       <div className="Counter">
+           <h1 onClick={this._resetCounter.bind(this)}>Counter value:  {this.state.counter}</h1>
+       </div>
+    );
+  }
+}
+
+
+
