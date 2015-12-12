dc067bee1bb80676266bed386db69ab87d1601d7
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-12 12:05:08 -0800

    refactor player component to not use state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = this.props.data
   }
   render() {
+    let data = this.props.data;
     return (
        <div className="Counter">
-           <h1 onClick={this.props.handleClick}> Player {this.props.data.name} with cards: {this.state.cards} </h1>
+           <h1 onClick={this.props.handleClick}> Player {data.name} with cards: {data.cards} </h1>
 
        </div>
     );
