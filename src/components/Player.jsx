5ab51b3f2a91537a2647de5d6011f56590e4f9a3
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-12 13:21:58 -0800

    print total inline

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -10,9 +10,8 @@ export default class App extends React.Component {
     let cardValues = _.sum(_.pluck(data.cards, 'value'));
     return (
        <div className="Player" style={{color: (cardValues > 21) ? 'red' : 'black'}}>
-           <h1 onClick={this.props.handleClick}> Player {data.name}
-             with cards:
-             {_.map(data.cards, (c) => c.name + " of " + c.suit + " ")}
+           <h1 onClick={this.props.handleClick}> Player {data.name} cards :
+             {_.map(data.cards, (c) => " " + c.name + " of " + c.suit + " ")} totaling: {cardValues}
            </h1>
        </div>
     );
