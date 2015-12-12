5f7eb1051b24d1dfcc60d3cefa1af3644e92d178
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-12 11:53:05 -0800

    give ability to add cards to player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = this.props.data
+  }
+  _addCard() {
+    this.setState({
+      cards: this.state.cards.concat(Math.floor(Math.random()*10))
+    })
   }
   render() {
     return (
        <div className="Counter">
-           <h1> Player {this.props.data.name} </h1>
+           <h1 onClick={this._addCard.bind(this)}> Player {this.props.data.name} with cards: {this.state.cards} </h1>
+
        </div>
     );
   }
